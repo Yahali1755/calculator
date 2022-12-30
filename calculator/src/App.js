9551ebbdcefdb4cad2347baa43421fd80ce74dd9
Yahali1212 <yahali100@gmail.com>
2022-12-30 13:30:50 +0200

    feat(calculator): calculator css

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,8 +1,48 @@
-import './App.css';
+import { Button, Grid } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+  app: {
+  },
+  button: { 
+    '&:hover': {
+      color: 'white',
+      borderColor: 'white'
+    },
+    color: 'black',
+    borderColor: 'black', 
+    borderRadius: '25%'
+
+  },
+  container: { 
+    marginTop: '25vh',
+    height: '50vh', 
+    width: '30vh', 
+    margin: 'auto',
+    borderRadius: '10%', 
+    backgroundColor: 'grey',
+  }
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.app}>
+        <Grid container className={classes.container}>
+          <Grid item xs={3}>
+            <Button variant='outlined' className={classes.button}> click on me </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Button variant='outlined' className={classes.button}> click on me </Button>
+          </Grid>
+          <Grid item xs={3} >
+            <Button variant='outlined' className={classes.button}> click on me </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Button variant='outlined' className={classes.button}> click on me </Button>
+          </Grid>
+        </Grid>
     </div>
   );
 }
