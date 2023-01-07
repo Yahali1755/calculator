7e8dd264391897e7521aa028a0a8094b9ed478e4
Yahali1212 <yahali100@gmail.com>
2023-01-07 14:59:26 +0200

    feat(calculator):  started adding calculator logic and more styles

--- calculator/src/components/displayPanel.jsx
+++ calculator/src/components/displayPanel.jsx
@@ -1,23 +1,3 @@
-import { Grid, TextField } from '@mui/material'
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles({
-    inputProps: {
-        height: '145px',
-        fontSize: '30px',
-        textAlign: 'center'
-    },
-    displayPanel: {
-      '& :disabled': {
-        WebkitTextFillColor: 'black'
-      },
-      boxSizing: 'border-box',
-      border: '1px solid black'
-    }
-});
-
-export const DisplayPanel  = () => {
-  const classes = useStyles();
 
   return (
     <Grid item>
