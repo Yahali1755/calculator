ed2730349850c31aeba081761c1ef9abcbe13c93
Yahali1212 <yahali100@gmail.com>
2023-07-14 13:29:32 +0300

    wip

--- src/displayPanel/displayPanel.tsx
+++ src/displayPanel/displayPanel.tsx
@@ -1,31 +1,23 @@
-import { Input } from '@mui/material'
+import { Typography } from '@mui/material'
 import { FC } from 'react';
 
 import { useCalculatorData } from '../contexts/calculatorDataContext';
 
 const styles = {
-  input: { 
-    textAlign: 'center' as const
-  },
   displayPanel: {
     fontSize: '1.5vw', 
     backgroundColor: 'white',
     border: '1px solid black',
     height: '100%',
-    width: '100%',
-    input: {
-      "&::placeholder": {
-         opacity: 1,
-      },
-   },
   }
-}
+};
 
 export const DisplayPanel: FC  = () => {
   const {equation, result} = useCalculatorData();
 
   return (
-      <Input readOnly disableUnderline autoFocus onBlur={e => e.target.focus()} placeholder={result} value={equation} 
-          inputProps={{ style: styles.input }} sx={styles.displayPanel}/>
+      <Typography display='flex' alignItems='center' justifyContent='center' sx={styles.displayPanel}>
+           {equation ? equation : result} 
+      </Typography>
   );
 };
